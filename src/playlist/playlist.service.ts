8fa8c77dc1574c8e4fae75ea32f93c7ec15f6031
tentenball <lth8905@gmail.com>
2023-11-17 01:17:35 +0900

    [Feat] : Create Playlist implement

--- src/playlist/playlist.service.ts
+++ src/playlist/playlist.service.ts
@@ -1,6 +1,14 @@
-import { Injectable, Logger } from "@nestjs/common";
+import {
+	HttpException,
+	HttpStatus,
+	Logger,
+	Request,
+	Response,
+	Injectable,
+} from "@nestjs/common";
 import { InjectModel } from "@nestjs/sequelize";
 import { Playlist } from "src/model/playlist.model";
+import { CreatePlaylistDto } from "./dto/create-playlist.dto";
 
 @Injectable()
 export class PlaylistService {
@@ -8,11 +16,39 @@ export class PlaylistService {
 
 	private readonly logger = new Logger("Playlist Service");
 
-	async findAll() {}
+	async findAll(@Request() req, @Response() res, playlist_id: string) {
+		console.log(playlist_id);
+	}
 
 	async findOne() {}
 
-	async create() {}
+	async createPlaylist(createPlaylist: CreatePlaylistDto) {
+		const functionName = PlaylistService.prototype.createPlaylist.name;
+		try {
+			const { title } = createPlaylist;
+			const duplicatedPlaylist = await this.playlistModel.findOne({
+				where: {
+					title: title,
+				},
+			});
+			if (duplicatedPlaylist) {
+				this.logger.error(`${functionName} : Duplicated Playlist Title`);
+				throw new HttpException(
+					"Duplicated Video Title",
+					HttpStatus.BAD_REQUEST,
+				);
+			}
+			const newPlaylist = await this.playlistModel.create({
+				title: title,
+			});
+			return newPlaylist;
+		} catch (error) {
+			this.logger.error(
+				`${functionName} : Error creating playlist - ${error.message}`,
+			);
+			throw new HttpException("Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
 	async update() {}
 
